41614f05a52609ef26d58ff5870fa9cd18201c32
Orkun Coskun <coskunorkun1@gmail.com>
2023-01-17 15:43:42 +0300

    Hatırlatma

---
+++ src/main/java/com/orkuncoskun/tutorials/JavaTutorials.java
@@ -0,0 +1,58 @@
+package com.orkuncoskun.tutorials;
+
+import java.util.Arrays;
+
+public class JavaTutorials {
+    public static void main(String[] args) {
+        //Arrays.sort();
+        //this.
+        //super
+        //break continue return
+        //optional
+        //cast
+        //primitive type
+        //wrapper class
+        //Random
+        //Math
+        //%s %d %f ==> printf()
+        //final
+        //static
+        //instanceof
+        //public private protected
+        //String
+        //StringBuilder
+        //StringTokenizer
+        //StringBuffer
+        //Enum
+        //Multithread
+        //S.O.L.I.D
+        //KISS
+        //DRY
+        //MONAD
+        //List Set Map
+        //Liste: LinkedList ArrayList
+        //Generic <>
+        //Iterator
+        //OOP
+        //interface
+        //abstract new
+        //inheritance
+        //UML - + __
+        //class
+        //POJO
+        //Bean
+        //Serileştirme
+        //IO
+        //JAR WAR
+        //Dağıtık programlama
+        //server
+        //constructor
+        //exception
+        //error
+        //Java 8 : stream,map,filter,sort
+        //Optional
+        //Lambda Expression
+        //Method References
+        //D.P.
+    }
+}
