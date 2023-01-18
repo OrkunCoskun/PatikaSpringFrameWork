2bda8ba5c0e36e218d407e2a3d0a153caf1f119c
Orkun Coskun <coskunorkun1@gmail.com>
2023-01-18 13:03:58 +0300

    @Produces

---
+++ src/main/java/com/orkuncoskun/cdi/_02_Produces.java
@@ -0,0 +1,23 @@
+package com.orkuncoskun.cdi;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
+
+@Named(value = "producetuto")
+@ApplicationScoped
+public class _02_Produces {
+
+    @Produces //üretiyor
+    public List<String> getList() {
+        List<String> list = new ArrayList<>();
+        list.add("Html5");
+        list.add("css3");
+        list.add("js");
+        list.add("react");
+        list.add("angular");
+        return list;
+    }
+}
